984d317f5d0c40872f379e4e8b4f5c3d91cafa41
Patrick Han <47380073+patrick-han@users.noreply.github.com>
2022-07-21 20:03:14 -0700

    Initial commit

---
+++ src/vk_engine.cpp
@@ -0,0 +1,123 @@
+﻿
+#include "vk_engine.h"
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+#include <vk_types.h>
+#include <vk_initializers.h>
+
+// bootstrap library
+#include "VkBootstrap.h"
+
+#include <iostream>
+
+//we want to immediately abort when there is an error. In normal engines this would give an error message to the user, or perform a dump of state.
+using namespace std;
+#define VK_CHECK(x)                                                 \
+	do                                                              \
+	{                                                               \
+		VkResult err = x;                                           \
+		if (err)                                                    \
+		{                                                           \
+			std::cout <<"Detected Vulkan error: " << err << std::endl; \
+			abort();                                                \
+		}                                                           \
+	} while (0)
+
+void VulkanEngine::init()
+{
+	// We initialize SDL and create a window with it. 
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+	
+	_window = SDL_CreateWindow(
+		"Vulkan Engine",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		_windowExtent.width,
+		_windowExtent.height,
+		window_flags
+	);
+
+	init_vulkan();
+	init_swapchain();
+	
+	//everything went fine
+	_isInitialized = true;
+}
+void VulkanEngine::cleanup()
+{	
+	if (_isInitialized) {
+		
+		SDL_DestroyWindow(_window);
+	}
+}
+
+void VulkanEngine::draw()
+{
+	//nothing yet
+}
+
+void VulkanEngine::run()
+{
+	SDL_Event e;
+	bool bQuit = false;
+
+	//main loop
+	while (!bQuit)
+	{
+		//Handle events on queue
+		while (SDL_PollEvent(&e) != 0)
+		{
+			//close the window when user alt-f4s or clicks the X button			
+			if (e.type == SDL_QUIT) bQuit = true;
+		}
+
+		draw();
+	}
+}
+
+void VulkanEngine::init_vulkan()
+{
+	// Build a VulkanInstance and store the debug messenger
+	vkb::InstanceBuilder builder;
+	auto inst_ret = builder.set_app_name("Vulkan Guide Engine")
+		.request_validation_layers() // Basic debug features
+		.require_api_version(1, 1, 0)
+		.use_default_debug_messenger()
+		.build();
+	vkb::Instance vkb_inst = inst_ret.value();
+	_instance = vkb_inst.instance;
+	_debug_messenger = vkb_inst.debug_messenger;
+
+	// Create surface from SDL window and create device from selected physical device
+	SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+	vkb::PhysicalDeviceSelector selector{ vkb_inst };
+	vkb::PhysicalDevice physicalDevice = selector
+		.set_minimum_version(1, 1)
+		.set_surface(_surface)
+		.select()
+		.value();
+	_chosenGPU = physicalDevice.physical_device;
+
+	vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+	_device = vkbDevice.device;
+
+}
+
+
+void VulkanEngine::init_swapchain()
+{
+	// Create the swapchain and its associated structures
+	vkb::SwapchainBuilder swapchainBuilder{ _chosenGPU, _device, _surface };
+	vkb::Swapchain vkbSwapchain = swapchainBuilder
+		.use_default_format_selection()
+		.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR) // vsync
+		.set_desired_extent(_windowExtent.width, _windowExtent.height)
+		.build()
+		.value();
+}
---
+++ src/vk_engine.h
@@ -0,0 +1,49 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+class VulkanEngine {
+public:
+
+	bool _isInitialized{ false };
+	int _frameNumber {0};
+
+	VkExtent2D _windowExtent{ 1700 , 900 };
+
+	struct SDL_Window* _window{ nullptr };
+
+	// Core Vulkan structures
+	VkInstance _instance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+	// Swapchain structures
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+
+
+	// Initializes everything in the engine
+	void init();
+
+	// Shuts down the engine
+	void cleanup();
+
+	// Draw loop
+	void draw();
+
+	// Run main loop
+	void run();
+
+	// Initialize core Vulkan structures
+	void init_vulkan();
+
+	// Initialize swapchain structures
+	void init_swapchain();
+};
